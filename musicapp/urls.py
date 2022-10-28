0cb92a10b7305b06a23bc54b11dabb5e8418f295
Daisy Rono <87646709+daisy2111-cheneno@users.noreply.github.com>
2022-10-28 17:16:47 +0300

    Delete urls.py

--- musicapp/urls.py
+++
@@ -1,9 +0,0 @@
-    
-from django.urls import path, re_path
-from . import views 
-
-urlpatterns = [    
-    path('', views.artiste, name='artists'),
-    path('song/<pk>/', views.song, name='songs'),
-    path('song/<pk>/lyric/<pk_1>/', views.lyric, name='lyrics'),
-]
